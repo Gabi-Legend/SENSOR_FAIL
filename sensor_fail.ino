603833dfd0f707242a2020d818474a96e56fdaf1
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-22 19:17:37 +0200

    DHT11 sensor added

--- sensor_fail.ino
+++ sensor_fail.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
 
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
+  if (isnan(t) || isnan(h)) {
+    Serial.println("DHT11 read error");
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" C  Humidity: ");
+    Serial.print(h);
+    Serial.println(" %");
+  }
+
+  delay(2000); // OBLIGATORIU
 }
