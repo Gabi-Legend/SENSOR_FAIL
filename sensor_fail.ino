127b25dbfd25c33dae9d2fb940523c18f8c2579f
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2026-01-22 19:24:38 +0200

    Led added

--- sensor_fail.ino
+++ sensor_fail.ino
@@ -4,25 +4,38 @@
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
+int LED = 3;
+
+bool lastState = LOW;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
   dht.begin();
 }
 
 void loop() {
   float h = dht.readHumidity();
   float t = dht.readTemperature();
-
+  bool currentState;
   if (isnan(t) || isnan(h)) {
     Serial.println("DHT11 read error");
+    currentState = LOW;
   } else {
+    currentState = HIGH;
     Serial.print("Temperature: ");
     Serial.print(t);
     Serial.print(" C  Humidity: ");
     Serial.print(h);
     Serial.println(" %");
   }
-
-  delay(2000); // OBLIGATORIU
+  if(currentState == HIGH && lastState == LOW){
+    digitalWrite(LED, HIGH);
+    lastState = HIGH;
+  }
+  else if(currentState == LOW && lastState == HIGH){
+    lastState = LOW;
+    digitalWrite(LED, LOW);
+  }
+  delay(2000); 
 }
